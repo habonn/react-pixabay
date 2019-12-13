7e8537593220668f686c37f0f009efa92e0d7759
habonn <teeruthabon@gmail.com>
2019-12-13 23:20:29 +0700

    second commit

---
+++ src/component/navbar/NavBar.js
@@ -0,0 +1,102 @@
+import React from 'react';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import IconButton from '@material-ui/core/IconButton';
+import Typography from '@material-ui/core/Typography';
+import { fade, makeStyles } from '@material-ui/core/styles';
+import FacebookIcon from '@material-ui/icons/Facebook';
+import TwitterIcon from '@material-ui/icons/Twitter';
+import InstagramIcon from '@material-ui/icons/Instagram';
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+    display: 'none',
+    [theme.breakpoints.up('sm')]: {
+      display: 'block',
+    },
+  },
+  search: {
+    position: 'relative',
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: fade(theme.palette.common.white, 0.15),
+    '&:hover': {
+      backgroundColor: fade(theme.palette.common.white, 0.25),
+    },
+    marginLeft: 0,
+    width: '100%',
+    [theme.breakpoints.up('md')]: {
+      marginLeft: theme.spacing(1),
+      width: 'auto',
+    },
+  },
+  searchIcon: {
+    width: theme.spacing(7),
+    height: '100%',
+    position: 'absolute',
+    pointerEvents: 'none',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  inputRoot: {
+    color: 'inherit',
+  },
+  inputInput: {
+    padding: theme.spacing(1, 1, 1, 7),
+    transition: theme.transitions.create('width'),
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+      width: 120,
+      '&:focus': {
+        width: 200,
+      },
+    },
+  },
+}));
+
+export default function SearchAppBar() {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography className={classes.title} variant="h3" noWrap>
+            <img src="https://image.flaticon.com/icons/svg/23/23346.svg" width="95" height="70"/>
+          </Typography>
+          <IconButton 
+          href="https://facebook.com"
+          edge="start"
+          color="inherit"
+          aria-label="open drawer">
+             <FacebookIcon />
+         
+          </IconButton>
+          <IconButton 
+          href="https://www.instagram.com"
+          edge="start"
+          color="inherit"
+          aria-label="open drawer">
+          <InstagramIcon/>
+         
+          </IconButton>
+          <IconButton 
+          href="https://twitter.com"
+          edge="start"
+          color="inherit"
+          aria-label="open drawer">
+          <TwitterIcon/>
+         
+          </IconButton>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+}
---
+++ src/component/search/Search.js
@@ -0,0 +1,115 @@
+import React, { Component } from 'react';
+import TextField from 'material-ui/TextField';
+import axios from 'axios';
+import ImageResults from '../image-results/ImageResult';
+import { fade, makeStyles } from '@material-ui/core/styles';
+
+
+class Search extends Component {
+
+  useStyles = makeStyles(theme => ({
+    root: {
+      flexGrow: 1,
+    },
+    menuButton: {
+      marginRight: theme.spacing(2),
+    },
+    title: {
+      flexGrow: 1,
+      display: 'none',
+      [theme.breakpoints.up('sm')]: {
+        display: 'block',
+      },
+    },
+    search: {
+      position: 'relative',
+      borderRadius: theme.shape.borderRadius,
+      backgroundColor: fade(theme.palette.common.white, 0.15),
+      '&:hover': {
+        backgroundColor: fade(theme.palette.common.white, 0.25),
+      },
+      marginLeft: 0,
+      width: '100%',
+      [theme.breakpoints.up('sm')]: {
+        marginLeft: theme.spacing(1),
+        width: 'auto',
+      },
+    },
+    searchIcon: {
+      width: theme.spacing(7),
+      height: '100%',
+      position: 'absolute',
+      pointerEvents: 'none',
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    inputRoot: {
+      color: 'inherit',
+    },
+    inputInput: {
+      padding: theme.spacing(1, 1, 1, 7),
+      transition: theme.transitions.create('width'),
+      width: '100%',
+      [theme.breakpoints.up('sm')]: {
+        width: 120,
+        '&:focus': {
+          width: 200,
+        },
+      },
+    },
+  }));
+  
+  state = {
+    searchText: '',
+    amount: 15,
+    apiUrl: 'https://pixabay.com/api',
+    apiKey: '14559322-2eadc7150b5627650e90eb161',
+    images: []
+  };
+
+  onTextChange = e => {
+    const val = e.target.value;
+    this.setState({ [e.target.name]: val }, () => {
+      if (val === '') {
+        this.setState({ images: [] });
+      } else {
+        axios
+          .get(
+            `${this.state.apiUrl}/?key=${this.state.apiKey}&q=${
+              this.state.searchText
+            }&image_type=photo&per_page=${this.state.amount}&safesearch=true`
+          )
+          .then(res => this.setState({ images: res.data.hits }))
+          .catch(err => console.log(err));
+      }
+    });
+  };
+
+  onAmountChange = (e, index, value) => this.setState({ amount: value });
+
+  render() {
+    console.log(this.state.images);
+    return (
+          <div>
+<TextField
+          name="searchText"
+          value={this.state.searchText}
+          onChange={this.onTextChange}
+          floatingLabelText="Search For Images"
+          fullWidth={true}
+        /> 
+                <br />
+
+        {this.state.images.length > 0 ? (
+          <ImageResults images={this.state.images} />
+        ) : null}
+          </div>
+          
+  
+
+    );
+  }
+}
+
+export default Search;
